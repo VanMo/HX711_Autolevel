545f41ed717b99947e7715910c76823af2f89f3a
VanMo <m@weintek.net>
2017-03-28 16:38:43 +0300

    Start

---
+++ HX711_Autolevel.ino
@@ -0,0 +1,84 @@
+
+#include "HX711.h"
+
+// HX711.DOUT	- pin #A1
+// HX711.PD_SCK	- pin #A0
+
+HX711 scale(A1, A0);		// parameter "gain" is ommited; the default value 128 is used by the library
+bool Detected;
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("HX711 Demo");
+
+  Serial.println("Before setting up the scale:");
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());			// print a raw reading from the ADC
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));  	// print the average of 20 readings from the ADC
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided 
+						// by the SCALE parameter (not set yet)  
+
+  scale.set_scale(10000.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();				        // reset the scale to 0
+
+  Serial.println("After setting up the scale:");
+
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());                 // print a raw reading from the ADC
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight, set with tare()
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided 
+						// by the SCALE parameter set with set_scale
+
+  Serial.println("Readings:");
+  //delay(2000);
+  //scale.tare();  
+}
+
+void loop() {
+  //Serial.print("one reading:\t");
+  //Serial.print(scale.get_units(), 1);
+  //Serial.print("\t| average:\t");
+  //Serial.println(scale.get_units(5), 1);
+
+  //scale.power_down();			        // put the ADC in sleep mode
+  //delay(500);
+  float value=scale.get_units(5);
+  Serial.println(value);
+  if(value>10)
+  {
+    digitalWrite(LED_BUILTIN, HIGH); 
+     if(!Detected)
+     {
+      Serial.print("Touched ");
+      Serial.println(value);
+      Detected=true;
+     }
+    
+  }else
+  {
+    digitalWrite(LED_BUILTIN, LOW); 
+    if(Detected)
+     {
+      Serial.println("Released");
+      Detected=false;
+      //delay(500);
+      //scale.tare();  
+     }
+  }
+  
+  //scale.power_up();
+}
