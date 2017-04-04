cb39c8fc47f348c1ada95a169bc9abd8e6efbf94
VanMo <m@weintek.net>
2017-04-04 14:54:41 +0300

    Добавлен сигнал тарировки

--- HX711_Autolevel.ino
+++ HX711_Autolevel.ino
@@ -3,62 +3,38 @@
 
 // HX711.DOUT	- pin #A1
 // HX711.PD_SCK	- pin #A0
-
+#define START_PIN 12
 HX711 scale(A1, A0);		// parameter "gain" is ommited; the default value 128 is used by the library
-bool Detected;
+bool Detected=false, Started=false;
 void setup() {
+  pinMode(START_PIN, INPUT_PULLUP);
+  
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
-  Serial.println("HX711 Demo");
-
-  Serial.println("Before setting up the scale:");
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());			// print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));  	// print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided 
-						// by the SCALE parameter (not set yet)  
 
   scale.set_scale(10000.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
-  scale.tare();				        // reset the scale to 0
+  //scale.tare();				        // reset the scale to 0
 
-  Serial.println("After setting up the scale:");
-
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());                 // print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight, set with tare()
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided 
-						// by the SCALE parameter set with set_scale
-
-  Serial.println("Readings:");
   //delay(2000);
   //scale.tare();  
 }
 
 void loop() {
-  //Serial.print("one reading:\t");
-  //Serial.print(scale.get_units(), 1);
-  //Serial.print("\t| average:\t");
-  //Serial.println(scale.get_units(5), 1);
-
-  //scale.power_down();			        // put the ADC in sleep mode
-  //delay(500);
+bool start;
+start=digitalRead(START_PIN);
+if(start)
+  {
+    if(!Started)
+    {
+      //scale.power_up();		
+      //delay(400);	        
+      Started=true;
+      scale.tare(10); 
+      Serial.println("Start"); 
+    }
   float value=scale.get_units(5);
-  Serial.println(value);
-  if(value>10)
+  //Serial.println(value);
+  if(value<-3)
   {
     digitalWrite(LED_BUILTIN, HIGH); 
      if(!Detected)
@@ -73,12 +49,25 @@ void loop() {
     digitalWrite(LED_BUILTIN, LOW); 
     if(Detected)
      {
-      Serial.println("Released");
+      Serial.print("Released ");
+      Serial.println(value);
       Detected=false;
       //delay(500);
       //scale.tare();  
      }
   }
-  
+  }else //end
+  {
+    if(Started)
+    {
+      Started=false;
+      Detected=false;
+      digitalWrite(LED_BUILTIN, LOW); 
+      Serial.println("Stop"); 
+      //scale.power_down();  // put the ADC in sleep mode
+    }
+    //float value=scale.get_units(5);
+    //Serial.println(value);
+  }
   //scale.power_up();
 }
