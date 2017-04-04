58d1a57096affabcd092309502859a621b6ee1aa
VanMo <m@weintek.net>
2017-04-04 15:06:06 +0300

    Почищено, добавлены параметры для настройки

--- HX711_Autolevel.ino
+++ HX711_Autolevel.ino
@@ -3,6 +3,8 @@
 
 // HX711.DOUT	- pin #A1
 // HX711.PD_SCK	- pin #A0
+//#define DEBUG //Выводить в порт текущие показания
+#define DETECT 3 //Порог срабатывания
 #define START_PIN 12
 HX711 scale(A1, A0);		// parameter "gain" is ommited; the default value 128 is used by the library
 bool Detected=false, Started=false;
@@ -15,8 +17,6 @@ void setup() {
   scale.set_scale(10000.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
   //scale.tare();				        // reset the scale to 0
 
-  //delay(2000);
-  //scale.tare();  
 }
 
 void loop() {
@@ -26,15 +26,15 @@ if(start)
   {
     if(!Started)
     {
-      //scale.power_up();		
-      //delay(400);	        
       Started=true;
       scale.tare(10); 
       Serial.println("Start"); 
     }
   float value=scale.get_units(5);
-  //Serial.println(value);
-  if(value<-3)
+  #ifdef DEBUG
+  Serial.println(value);
+  #endif
+  if(value<-DETECT || value>DETECT)
   {
     digitalWrite(LED_BUILTIN, HIGH); 
      if(!Detected)
@@ -52,8 +52,6 @@ if(start)
       Serial.print("Released ");
       Serial.println(value);
       Detected=false;
-      //delay(500);
-      //scale.tare();  
      }
   }
   }else //end
@@ -64,10 +62,7 @@ if(start)
       Detected=false;
       digitalWrite(LED_BUILTIN, LOW); 
       Serial.println("Stop"); 
-      //scale.power_down();  // put the ADC in sleep mode
     }
-    //float value=scale.get_units(5);
-    //Serial.println(value);
   }
   //scale.power_up();
 }
